8944dc5a5cc4ec6903b3a56635522e32c5706527
LissAponte <japonterivera@student.neumont.edu>
2026-02-17 11:15:40 -0700

    First commit

---
+++ src/main/java/org/example/portfolio_eng210/PortfolioEng210Application.java
@@ -0,0 +1,13 @@
+package org.example.portfolio_eng210;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioEng210Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioEng210Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/portfolio_eng210/PortfolioEng210ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.portfolio_eng210;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioEng210ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
